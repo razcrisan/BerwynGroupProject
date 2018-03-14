5bf7f1bb18ab67bfe99f79a4a099de266a8946b7
Razvan Crisan <rcrisan13@gmail.com>
2018-03-14 16:45:18 -0400

    added number of total lines, cleaned up project

--- Program.cs
+++ Program.cs
@@ -19,36 +19,26 @@ namespace BerwynGroupProject
         public string Val3 {get; set;}
         
 
-//         public static Values FromLine(string line)
-//         {
-//             var data = line.Split(',');
-
-//             return new Values()
-//             {
-//                 GUID = data[0],
-//                 Val1 = int.Parse(data[1]),
-//                 Val2 = int.Parse(data[2]),
-//                 Val3 = data[3]
-//             };
-//         }
-     
-//         }
-        
-//         static IList<Values> ReadValues(string path)
-//         {
-           
-            
-//             var list = new List<Values>();
-//             foreach(var line in File.ReadLines(path))
-//             {
-//                 int lines = File.ReadAllLines("../BerwynGroupProject/test.csv").Length;
-//             }
-//             return list;
-//         }
-        
-//     }
 
         //method to put GUID values into an array
+        public Array ArrayOfGUID(string[] args)
+        {
+            string filePath = @"../BerwynGroupProject/test.csv";
+            StreamReader sr = new StreamReader(filePath);
+            var lines = new List<string[]>();
+            int Row = 0;
+            while (!sr.EndOfStream)
+            {
+                string[] Line = sr.ReadLine().Split(',');
+                lines.Add(Line);
+                Row++;
+                Console.WriteLine(Row);
+            }
+            var data = lines.ToArray();
+            return data;
+
+        }
+        }
         //method to put Val1 values into an array
         //method to put Val2 values into an array
         //method to put Val3 values into an array
@@ -91,62 +81,6 @@ namespace BerwynGroupProject
 
             int firstNum = File.ReadAllLines("../BerwynGroupProject/test.csv").Length;
 
-            
-
-
         }
     }
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-// Different Method of parsing. 
-        // struct Vertex
-        // {
-        //     private int _x, _y, _z;
-
-        //     public Vertex (int x, int y, int z)
-        //     {
-        //         _x = x;
-        //         _z = z;
-        //         _y = y;
-        //     }
-        //     public int X {get {return _x;}}
-        //     public int Y {get {return _y;}}
-        //     public int Z {get {return _z;}}
-
-        //     public override string ToString()
-        //     {
-        //         return String.Format("{0}, {1}, {2}", _x, _y, _z);
-        //     }
-
-        // public static bool Parse(string text, out Vertex vertex)
-        // {
-        //     const string PATTERN = @"(?<X>\d+), (?<Y>\d+), (?<Z>\d+)";
-        //     Match match = Regex.Match(text, PATTERN);
-
-        //     if (match.Success)
-        //     {
-        //         int x = int.Parse(match.Groups["X"].Value);
-        //         int y = int.Parse(match.Groups["Y"].Value);
-        //         int z = int.Parse(match.Groups["Z"].Value);
-        //         vertex = new Vertex (x, y, z);
-        //         return true;
-        //     }
-        //     else
-        //     {
-        //         vertex = new Vertex();
-        //         return false;
-        //     }
-        //}
-    //}
-//}
